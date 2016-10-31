c33c3f96116a6bc3a703ba3fbf64a871c48a1944
Georgia Mills <georgee_123@hotmail.com>
2016-10-31 16:49:29 +0000

    added actionCreator for clicking a book title

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import {bindActionCreators } from 'redux';
 
 class BookList extends Component {
-  render List(){
-    return.this.props.books.map((book) => {
-      return(
-        <li key={book.title} className="list-group-item">{book.title}>
+  renderList(){
+    return this.props.books.map((book) => {
+      return (
+        <li
+          key={book.title}
+          onClick={() => this.props.selectBook(book)}
+          className="list-group-item">
+          {book.title}
         </li>
       )
     });
@@ -26,4 +32,11 @@ function mapStateToProps(state) {
   }
 }
 
-export default connect(mapStateToProps)(BookList);
+function mapDispaceToProps(dispatch) {
+  //whenever selectBook is called,
+  //the result should be passed to all of our reducers
+  //The dispatch takes the action and passes them to the reducers
+  return bindActionCreators({ selectBook: selectBook }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispaceToProps)(BookList);
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function () {
   return [
-    {title: 'Javascript: Ther Good Parts '}
-    {title: 'harry potter'}
-    {title: 'Poo-Dr'}
-    {title: 'Eloquent Ruby'}
+    {title: 'Javascript: Ther Good Parts '},
+    {title: 'harry potter'},
+    {title: 'Poo-Dr'},
+    {title: 'Eloquent Ruby'},
   ]
 }
